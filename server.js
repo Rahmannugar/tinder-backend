b6f45151d7f1cabe09eb8033f76b51d0e8c1e874
Nugar <cladeadenugar@gmail.com>
2023-11-02 18:31:46 +0100

    added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Cards = require("./cardsDB");
 require("dotenv").config();
+const cors = require("cors");
 
 //App config
 const app = express();
@@ -10,6 +11,8 @@ const mongoUrl =
   "mongodb+srv://Rahmannugar:Nugarcladex@cluster0.s6blitt.mongodb.net/?retryWrites=true&w=majority";
 
 //Middlewares
+app.use(express.json());
+app.use(cors());
 
 //DB config
 mongoose.connect(mongoUrl, {
